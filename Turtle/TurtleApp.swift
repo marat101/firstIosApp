0a960c1f31d379bea4e33c4033291fe8027fb451
marat101 <106337953+marat101@users.noreply.github.com>
2023-07-20 02:55:20 +0300

    Initial Commit

---
+++ Turtle/TurtleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurtleApp.swift
+//  Turtle
+//
+//  Created by Марат Наврузаиев on 20.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TurtleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
